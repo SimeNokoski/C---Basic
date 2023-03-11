4b0826b0596ec65533c300160a57f64342e9223f
SimeNokoski <simenokoski99@hotmail.com>
2023-03-11 03:01:52 +0100

    Homework-1

---
+++ homework-1.NET/Homework-1/AverageNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter the First number:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Second number:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the  third number:");
+double num3 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the four number:");
+double num4 = Convert.ToDouble(Console.ReadLine());
+
+double average = (num1 + num2 + num3 + num4) / 4;
+Console.WriteLine("The average of " + num1 + ", " + num2 + ", " + num3 + " and " + num4 + " is: " + average);
+
+ 
+
+
+
+
+
---
+++ homework-1.NET/Homework-1/RealCalculator/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter the First number");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Second number");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter the Operation(+,-,*,/)");
+string operation = Console.ReadLine();
+
+double result;
+if(operation == "+")
+{
+    result = num1 + num2;
+    Console.WriteLine("The result is: " + result);
+}
+else if(operation == "-")
+{
+    result = num1 - num2;
+    Console.WriteLine("The result is: " + result);
+}
+else if (operation == "*")
+{
+    result = num1 * num2;
+    Console.WriteLine("The result is: " + result);
+}
+else if (operation == "/")
+{
+    result = num1 / num2;
+    Console.WriteLine("The result is: " + result);
+}
+else
+{
+    Console.WriteLine("Invalid operation");
+}
